02145333a9c713fe4eff3f5e5104a8e57811c99b
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-02-08 21:14:54 -0600

    Added Photon 2 system.
    Created Photon Lobby script, we are now able to load into a random room and also able to join an existing room. This is part 3 of the tutorial.

---
+++ FPS(projectGame)/Assets/PhotonLobby.cs
@@ -0,0 +1,75 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+    public GameObject battleButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this; //creates the singleton, lives with in the Main menu screen.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Connects to Master photon server.
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        battleButton.SetActive(true);
+    }
+
+    public void OnBattleButtonClick()
+    {
+        Debug.Log("Battle Button was clicked");
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random game but failed. There must be no open games available");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Trying to create a new room");
+        int randomRoomName = Random.Range(0, 1000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 10 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("We are now in a room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed, there must already be aroom with the same name");
+        CreateRoom();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("Cancel Button was clicked");
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
