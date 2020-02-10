05f7080474aa973eecf837e18a647b6ed4529f5e
Jon50 <isql550@gmail.com>
2020-02-10 20:58:37 +0100

    Character Selection
    Tutorial Part 8

---
+++ FPS(projectGame)/Assets/Scripts/AvatarSetup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class AvatarSetup : MonoBehaviour
+{
+    private PhotonView PV;
+    public int characterValue;
+    public GameObject myCharacter;
+
+    private void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        if (PV.IsMine)
+        {
+            PV.RPC("RPC_AddCharacter", RpcTarget.AllBuffered, PlayerInfo.PI.mySelectedCharacter);
+        }
+    }
+
+    [PunRPC]
+    void RPC_AddCharacter(int whichCharacter)
+    {
+        characterValue = whichCharacter;
+        myCharacter = Instantiate(PlayerInfo.PI.allCharacters[whichCharacter], transform.position, transform.rotation, transform);
+    }
+}
