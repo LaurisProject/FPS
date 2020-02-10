561c9451ab81cf0219d7207c65fea9bc5a6d1ea2
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-02-09 22:40:25 -0600

    Added player avatar, added game controller folder, added gamesetup , added photon player script,added player move script, added
    this is to video 7 need to start on 8, player has movement terrain is set in prefab, if you have problems loading terrain add it in the inspector and then apply to prefab.

---
+++ FPS(projectGame)/Assets/Scripts/Game Controller/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PhotonView PV;
+    private CharacterController myCC;
+    public float movementSpeed;
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        myCC = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (PV.IsMine)
+        {
+            BasicMovement();
+            BasicRotation();
+        }
+    }
+    void BasicMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            myCC.Move(transform.forward * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            myCC.Move(-transform.right * Time.deltaTime * movementSpeed);//please pay attention to the negative sign in front of the transform.
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myCC.Move(transform.right * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            myCC.Move(-transform.forward * Time.deltaTime * movementSpeed);//please pay attention to the negative sign in front of the transform.
+        }
+    }
+
+    void BasicRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+        transform.Rotate(new Vector3(0, mouseX, 0));
+
+    }
+}
---
+++ FPS(projectGame)/Assets/Scripts/MultiPlayerSettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiPlayerSettings : MonoBehaviour
+
+{
+    
+    public static MultiPlayerSettings multiplayerSetting;
+
+    //determines the delay type of the game
+
+    public bool delayStart;
+    public int maxPlayers;
+
+    public int menuScene;
+
+    // To call scenes from our scripts
+    public int multiplayerScene;
+    
+    private void Awake()
+    {
+        if(MultiPlayerSettings.multiplayerSetting == null)
+        {
+            // If settings are not null
+            MultiPlayerSettings.multiplayerSetting = this;
+        }
+        else
+        {
+            if(MultiPlayerSettings.multiplayerSetting != this)
+            {
+                // Then delete the game object if it doesnt match with instance
+                Destroy(this.gameObject);
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
